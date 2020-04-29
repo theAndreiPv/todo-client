6cd4495b0083a29a07b022db672c4f0be5bfb67f
Andrei Popov <the.andrei.pv@gmail.com>
2020-04-29 07:05:55 +0300

    Исправил ошибку: в моб.сайдбаре не рендерилась высота компонентов textarea

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -9,8 +9,16 @@ aside(
     TaskCheckbox(completed)
   ContainerScroll(class='flex-grow')
     div(class='px-6 py-4')
-      TextareaAutosize(rows='1' class='block w-full mb-4 font-bold text-6' value='Обучение веб 6h' )
-      TextareaAutosize(rows='1' class='block w-full leading-6 placeholder-black-30' placeholder='Description' value='')
+      TextareaAutosize(
+        :key='classContainer'
+        rows='1'
+        class='block w-full mb-4 font-bold text-6'
+        value='Обучение веб 6h')
+      TextareaAutosize(
+        :key='classContainer'
+        rows='1'
+        class='block w-full leading-6 placeholder-black-30'
+        placeholder='Description' value='')
   footer(class='flex items-center flex-shrink-0 h-12 px-6 border-t')
     button
       BaseSvg(name='delete' class='w-6 h-6 text-black-30 hover:text-black-50')
